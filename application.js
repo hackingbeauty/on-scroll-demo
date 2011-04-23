26d6cffc3655221467a00e451398189145a36cd0
Mark Muskardin <markmuskardin@gmail.com>
2011-04-23 16:23:07 -0700

    first commit

---
+++ application.js
@@ -0,0 +1,11 @@
+ScrollDemo = {
+  init:function(){
+    var elemId = document.getElementById('merchFooter');
+    this.loadWhenViewable(elemId);
+  },
+  loadWhenViewable:function(elemId){
+
+  }
+}
+
+ScrollDemo.init();
