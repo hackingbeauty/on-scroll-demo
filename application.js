065a73a96a9eacf2ea7b72dee1b562309d54c287
Mark Muskardin <markmuskardin@gmail.com>
2011-04-23 17:23:14 -0700

    added alert once element is in view - only tested in FF - now we have to test in all browsers

--- application.js
+++ application.js
@@ -11,8 +11,13 @@ ScrollDemo = {
       // If the scrollLeft/Top properties are provided on either the document.body or document.documentElement, they are reliable in everything except older Safari and OmniWeb 4.5+, which return -8 if the scrolling is 0
       // reference => http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
       var top = (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-      console.log(ScrollDemo.findPos(elemId));
-      console.log("offsetLeft: " + elemId.offsetLeft + " offsetTop: " + elemId.offsetTop);
+      //console.log("offsetLeft: " + elemId.offsetLeft + " offsetTop: " + elemId.offsetTop);
+      var vpH = ScrollDemo.vpHeight();
+      var coverage = parseInt(vpH + top);
+      //console.log("coverage is: " + coverage);
+      if (coverage >= elemId.offsetTop){
+        console.log("it is there!");
+      }
     }//end window.onscroll
   },
   findPos:function(obj){ // source: http://www.quirksmode.org/js/findpos.html
