750e26d19e191f77e2e52cf7b699645e287d6508
Mark Muskardin <markmuskardin@gmail.com>
2011-04-23 17:14:37 -0700

    added method to calculate viewport - only seems to work in FF right now

--- application.js
+++ application.js
@@ -2,10 +2,11 @@ ScrollDemo = {
   init:function(){
     var elemId = document.getElementById('merchFooter');
     this.loadWhenViewable(elemId);
+    console.log("viewPort height: " + this.vpHeight());
   },
-  loadWhenViewable:function(elemId){
+  loadWhenViewable:function(elemId){ // source: http://www.dustindiaz.com/element-scroll-into-view/
     window.onscroll = function(){
-      //Notes.  "document" is the JavaScript object that represents the container for all HTML HEAD and BODY object/
+      // Notes.  "document" is the JavaScript object that represents the container for all HTML HEAD and BODY object/
       //"documentElement" is a property on the "document" object that represents the root element a.k.a. the <html> tag
       // If the scrollLeft/Top properties are provided on either the document.body or document.documentElement, they are reliable in everything except older Safari and OmniWeb 4.5+, which return -8 if the scrolling is 0
       // reference => http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
@@ -23,6 +24,9 @@ ScrollDemo = {
       } while (obj = obj.offsetParent);    
        return [curleft,curtop];
     }
+  },
+  vpHeight:function() { // source: http://javascript.about.com/od/browserobjectmodel/a/bom10.htm
+    return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   }
 }
 
